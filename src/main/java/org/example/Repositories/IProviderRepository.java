0b0a519422a91affdb1b2bf7e6455af0e74f5728
DimasVeliz <trevor90094@yandex.com>
2023-04-19 19:18:50 +0200

    Repository annotation added for autowiring

--- src/main/java/org/example/Repositories/IProviderRepository.java
+++ src/main/java/org/example/Repositories/IProviderRepository.java
@@ -2,9 +2,11 @@ package org.example.Repositories;
 
 import org.example.Entities.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IProviderRepository extends JpaRepository<Provider,Integer> {
     List<Provider> findByClientId(int client_id);
 }
