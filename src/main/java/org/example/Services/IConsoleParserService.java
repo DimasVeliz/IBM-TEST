7c19ccdccbbf9fd92b984b1081e1fb16db2c2941
DimasVeliz <trevor90094@yandex.com>
2023-04-19 19:17:06 +0200

    ConsoleParserServiceImpl now can save to file and parse the client_id

--- src/main/java/org/example/Services/IConsoleParserService.java
+++ src/main/java/org/example/Services/IConsoleParserService.java
@@ -1,4 +1,11 @@
 package org.example.Services;
 
+import org.example.Dtos.ProviderDto;
+
+import java.util.List;
+
 public interface IConsoleParserService {
+    boolean saveToFile(List<ProviderDto> response);
+
+    int resolveClientId(String[] args);
 }
--- src/main/java/org/example/Services/Impl/ConsoleParserServiceImpl.java
+++ src/main/java/org/example/Services/Impl/ConsoleParserServiceImpl.java
@@ -1,6 +1,40 @@
 package org.example.Services.Impl;
 
+import org.example.Dtos.ProviderDto;
 import org.example.Services.IConsoleParserService;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
+@Service
 public class ConsoleParserServiceImpl implements IConsoleParserService {
+
+    @Override
+    public boolean saveToFile(List<ProviderDto> response) {
+        String FILE_NAME = "response.txt";
+        File file = new File(FILE_NAME);
+        try {
+
+            FileWriter writer = new FileWriter(file);
+            String HEADERS = "Id,Name,Registration Date, Client Id\n";
+            writer.write(HEADERS);
+            for (ProviderDto provider : response) {
+                writer.write(provider.getName() + "," + provider.getRegistration_date() + "," + provider.getClient_id() + "\n");
+            }
+
+            writer.close();
+            return true;
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public int resolveClientId(String[] args) {
+        if(args.length!=1)return -1;
+        return Integer.parseInt(args[0]);
+    }
 }
