5459f93ebef1067517ba2a0bc55007ce2e05dc13
DimasVeliz <trevor90094@yandex.com>
2023-04-19 16:57:42 +0200

    Provider Entity and Repository Created

--- src/main/java/org/example/Entities/Provider.java
+++ src/main/java/org/example/Entities/Provider.java
@@ -1,4 +1,29 @@
 package org.example.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "provider")
 public class Provider {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY )
+    @Column(name="id")
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Date registration_date;
+
+    @Column
+    private int client_id;
 }
--- src/main/java/org/example/Repositories/IProviderRepository.java
+++ src/main/java/org/example/Repositories/IProviderRepository.java
@@ -1,6 +1,7 @@
 package org.example.Repositories;
 
 import org.example.Entities.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IProviderRepository {
+public interface IProviderRepository extends JpaRepository<Provider,Integer> {
 }
