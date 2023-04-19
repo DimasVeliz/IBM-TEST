f67d0a8619cc7bea5dc1e05132186b6d98166e67
DimasVeliz <trevor90094@yandex.com>
2023-04-19 17:09:19 +0200

    ProviderServiceImpl uses extended functionality of IProviderRepository to get the provider by client_id

--- src/main/java/org/example/Services/IProviderService.java
+++ src/main/java/org/example/Services/IProviderService.java
@@ -1,4 +1,10 @@
 package org.example.Services;
 
+import org.example.Dtos.ProviderDto;
+
+import java.util.List;
+
 public interface IProviderService {
+
+    List<ProviderDto> getProviderByClientId(int client_id);
 }
--- src/main/java/org/example/Services/Impl/ProviderServiceImpl.java
+++ src/main/java/org/example/Services/Impl/ProviderServiceImpl.java
@@ -1,6 +1,28 @@
 package org.example.Services.Impl;
 
+import lombok.RequiredArgsConstructor;
+import org.example.Dtos.ProviderDto;
+import org.example.Entities.Provider;
+import org.example.Repositories.IProviderRepository;
 import org.example.Services.IProviderService;
 
+import java.util.Collections;
+import java.util.List;
+
+@RequiredArgsConstructor
 public class ProviderServiceImpl implements IProviderService {
+
+    private final IProviderRepository providerRepository;
+    @Override
+    public List<ProviderDto> getProviderByClientId(int client_id) {
+        List<Provider> response = providerRepository.findByClientId(client_id);
+        return response.isEmpty()?
+                Collections.emptyList():
+                response
+                        .stream()
+                        .map(provider -> new ProviderDto(provider.getName(),
+                                provider.getRegistration_date(),
+                                provider.getClient_id()))
+                        .toList();
+    }
 }
