28e7fea09d718436b89a3a2f38efe6ed23bb6d0c
DimasVeliz <trevor90094@yandex.com>
2023-04-19 17:07:23 +0200

    ProviderRepository Extended

--- src/main/java/org/example/Repositories/IProviderRepository.java
+++ src/main/java/org/example/Repositories/IProviderRepository.java
@@ -3,5 +3,8 @@ package org.example.Repositories;
 import org.example.Entities.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IProviderRepository extends JpaRepository<Provider,Integer> {
+    List<Provider> findByClientId(int client_id);
 }
